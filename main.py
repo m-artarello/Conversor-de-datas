9545f9e84958af11f459cfe78f386c824731e301
Matheus Martarello <matheus1.martarello@gmail.com>
2023-03-26 21:00:16 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import re
+
+# Abrir o arquivo SQL em modo leitura
+# O encoding pode variar de acordo com o arquivo
+with open('C:/Users/Matheus/Downloads/original.sql', 'r', encoding='iso-8859-1') as f:
+    sql = f.read()
+
+    # Expressão regular para encontrar datas no formato m/d/yyyy ou mm/dd/yyyy
+    pattern = r"'(\d{1,2})/(\d{1,2})/(\d{4})'"
+
+    # Substituir as datas no arquivo SQL pelo formato ano-mes-dia
+    sql = re.sub(pattern, lambda match: f"'{match.group(3)}-{match.group(1).zfill(2)}-{match.group(2).zfill(2)}'", sql)
+
+# Abrir o novo arquivo SQL em modo escrita
+with open('C:/Users/Matheus/Downloads/novo.sql', 'w', encoding='utf-8') as f:
+    # Escrever o SQL atualizado no arquivo novo.sql
+    f.write(sql)
